8377b04d57c30d9caa24883c9b4f0978cbcb87e4
AhmadShekibHaidari <ahmadshekibhaidari82@gmail.com>
2024-10-20 17:35:25 +0330

    WE CREATE MAIN CLASS THEN WE ADD ELONE MASK AND MARK AS STUDENT

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Elone","Mask","4023144",19);
+        Student s2 = new Student("mark","Zakerberg","4023145",18);
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+}
